39cb29f12767b9b8886760f851893cf5ec7e20bd
vineet-cyberkan <vineet1.st@gmail.com>
2019-08-22 15:11:00 +0530

    changing popup component from html to jsx

--- src/Popup.js
+++ src/Popup.js
@@ -6,24 +6,26 @@ function Popup() {
   const [lgShow, setLgShow] = useState(false);
 
   return (
-    <ButtonToolbar>
-      <Button onClick={() => setLgShow(true)}>view our e-catalog</Button>
+  	//<a href="javascript:void(0)" className="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">view our e-catalog</a>
+  	//<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">view our e-catalog</button>
 
-      <Modal
-        size="lg"
-        show={lgShow}
-        onHide={() => setLgShow(false)}
-        aria-labelledby="example-modal-sizes-title-lg">
-
-        <Modal.Header closeButton>
-          <Modal.Title id="example-modal-sizes-title-lg">
-            Large Modal
-          </Modal.Title>
-        </Modal.Header>
-
-        <Modal.Body>...</Modal.Body>
-      </Modal>
-    </ButtonToolbar>
+    <div className="modal fade" tabIndex="-1" role="dialog">
+	  <div className="modal-dialog" role="document">
+	    <div className="modal-content">
+	      <div className="modal-header">
+	        <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	        <h4 className="modal-title">Modal title</h4>
+	      </div>
+	      <div className="modal-body">
+	        <p>One fine body&hellip;</p>
+	      </div>
+	      <div className="modal-footer">
+	        <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+	        <button type="button" className="btn btn-primary">Save changes</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
   );
 }
 
